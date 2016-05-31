95468345d32adfaac7ccc16fc0f46fc7193643a5
FireController1847 <usfirepilot123@gmail.com>
2016-05-31 17:58:50 -0600

    Create InitItems.java

---
+++ Basic-Items/src/main/java/com.fire/tutorialmod/Init/InitItems.java
@@ -0,0 +1,31 @@
+package com.fire.tutorialmod.Init;
+
+import com.fire.tutorialmod.Items.itemTest.itemTest;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class InitItems {
+	
+	public static Item itemTest;
+
+	public static void init(){
+		itemTest = new itemTest("itemTest").setCreativeTab(CreativeTabs.tabMisc);
+		register();
+	}
+	
+	public static void register(){
+		GameRegistry.register(itemTest);
+	}
+	
+	public static void registerRenders(){
+		registerRender(itemTest);
+	}
+	
+	public static void registerRender(Item item){
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+	}
+}
