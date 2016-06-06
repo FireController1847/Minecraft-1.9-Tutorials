4c7a464438262a5048b1ffad3d4add5a6745c41e
FireController1847 <usfirepilot123@gmail.com>
2016-06-05 20:35:31 -0600

    Create Strings.java

---
+++ Basic-Blocks/src/main/java/com.fire/tutorialmod/Strings.java
@@ -0,0 +1,10 @@
+package com.fire.tutorialmod;
+
+public class Strings {
+	public static final String MODID = "tutorialmod";
+	public static final String NAME = "Tutorial Mod";
+	public static final String VERSION = "0.0.1 Beta";
+	
+	public static final String COMMONPROXY = "com.fire.tutorialmod.Proxies.CommonProxy";
+	public static final String CLIENTPROXY = "com.fire.tutorialmod.Proxies.ClientProxy";
+}
